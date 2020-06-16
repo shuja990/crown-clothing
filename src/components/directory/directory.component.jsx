faf2df68dc8ba0bfc4cbe1b510db93ae30655448
shuja990 <shujaali1234@gmail.com>
2020-06-16 20:15:11 +0500

    updating our app

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './directory.styles.scss'
+import MenuItem from '../menu-item/menu-item.component'
+import SECTIONS_DATA from "./sections.data.js";
+class Directory extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+          sections: SECTIONS_DATA
+        }
+    }
+    render(){
+        return(
+            <div className="directory-menu">
+                {
+                    this.state.sections.map(({id, ...otherSectionProps}) =>(
+                        <MenuItem key={id} {...otherSectionProps}/>
+                    ))
+                }
+            </div>
+        )
+    }
+}
+export default Directory;
