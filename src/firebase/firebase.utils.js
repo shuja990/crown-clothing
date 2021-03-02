e3b8a0b6941716390dbe6eff966ee3cd64de08e1
shuja990 <58367243+shuja990@users.noreply.github.com>
2021-03-03 01:52:53 +0500

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-        apiKey: "AIzaSyAe3FLVJ0d-0BBw5kwD_6Bfrz_ySxGCc6A",
-        authDomain: "crown-db-9ee0b.firebaseapp.com",
-        databaseURL: "https://crown-db-9ee0b.firebaseio.com",
-        projectId: "crown-db-9ee0b",
-        storageBucket: "crown-db-9ee0b.appspot.com",
-        messagingSenderId: "165687828593",
-        appId: "1:165687828593:web:879c4ac2d61623940afbd0",
-        measurementId: "G-X2VV5P0HG6"
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
 }
 export const createUserProfileDocument = async (userAuth,additionalData) => {
     if(!userAuth) return;
